6be2487c73735a14650dfe3ae1b1b7c9e19a3326
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-04-29 07:49:47 -0600

    creando mock de datos

--- projects/JSONPLACEHOLDER/src/App.jsx
+++ projects/JSONPLACEHOLDER/src/App.jsx
@@ -1,40 +1,10 @@
-import { Button } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomeScreen } from "./pages/HomeScreen";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="bg-slate-700 h-screen flex-col text-center justify-center ">
-              <h1 className="text-3xl font-bold underline text-cyan-300">
-                Hello world!
-              </h1>
-              <button className="bg-sky-500 hover:bg-sky-700 px-5 py-5 my-4 text-white rounded-lg">
-                Mi primer boton
-              </button>
-              <Button
-                variant="contained"
-                size="large"
-                color="primary"
-                className="bg-sky-500 hover:bg-sky-700 px-5 py-5 my-4 text-white rounded-lg"
-              >
-                Contained
-              </Button>
-            </div>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <div className="bg-slate-700 h-screen flex-col text-center justify-center ">
-              <h1 className="text-3xl font-bold underline text-cyan-300">
-                Hello about!
-              </h1>
-            </div>
-          }
-        />
+        <Route path="/" element={<HomeScreen />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ projects/JSONPLACEHOLDER/src/pages/HomeScreen.jsx
@@ -0,0 +1,33 @@
+import responsePosts from "../mocks/withResponse.json";
+export function HomeScreen() {
+  const posts = responsePosts;
+  const hasPosts = posts.length > 0;
+  return (
+    <div className="bg-slate-700 h-full flex-col items-center text-center py-8">
+      <header>
+        <h1 className="text-4xl text-sky-500">JsonPlaceHolder</h1>
+      </header>
+      <main className="flex flex-col items-center w-full ">
+        {hasPosts ? (
+          <ul className="text-left text-white max-w-4xl mx-auto">
+            {posts.map((post) => (
+              <li key={post.id} className="m-3">
+                <h2 className="text-2xl text-yellow-300">{post.id}</h2>
+                <h3>
+                  <span className="text-emerald-300 text-xl">Titulo:</span>
+                  {post.title}
+                </h3>
+                <p>
+                  <span className="text-cyan-500 text-lg">Body:</span>
+                  {post.body}
+                </p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No post encontrados</p>
+        )}
+      </main>
+    </div>
+  );
+}
